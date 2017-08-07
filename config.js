c813b9f5731fd536c527f1d3f797bb2f4e5c5499
TheRoyalTnetennba <gpaye8@gmail.com>
2017-08-07 12:04:17 -0700

    reworked dependencies

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 import bluebird from 'bluebird';
 import mongoose from 'mongoose';
 
+const production = true;
+
+// const dbURL = production ? 'mongodb://<dbuser>:<dbpassword>@ds129143.mlab.com:29143/kitchenfox' : 
+
 mongoose.Promise = bluebird;
 mongoose.connect('mongodb://localhost:27017/kitchenFox');
 
